1a392efd865cd43819ba4ceedb4be219f73bfd39
rohit-iwnl <rohitmanivel9@gmail.com>
2024-03-12 17:18:29 -0700

    282A_Bits++

---
+++ 282A_Bits.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int X=0;
+    string s;
+    while(n--){
+        cin>>s;
+        if(s=="++X" || s=="X++") X++;
+        else X--;
+    }
+
+    cout<<X;
+
+}
